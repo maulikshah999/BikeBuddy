0697432500e57539417af0de886f822314e6bc2d
SFLDPGUSER-24 <maulik.shah9999@gmail.com>
2018-03-02 14:26:50 -0500

    Created the POJO and Connection classes

--- app/src/main/java/com/example/sfldpguser_24/model/BikeBuddyImplOkHttp.java
+++ app/src/main/java/com/example/sfldpguser_24/model/BikeBuddyImplOkHttp.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -22,9 +23,11 @@ public class BikeBuddyImplOkHttp {
         OkHttpClient httpclient = new OkHttpClient();
         Request request = new Request.Builder().url(URI_BIKES).get().build();
         try (Response response = httpclient.newCall(request).execute()) {
+
             ObjectMapper mapper = new ObjectMapper();
+            System.out.println("response"+response.body().string());
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            bikes = mapper.readValue(response.body().bytes(), Bikes[].class);
+           // bikes = mapper.readValue(response.body().bytes(), Bikes[].class);
         }
         return bikes;
     }
@@ -32,18 +35,33 @@ public class BikeBuddyImplOkHttp {
         BikeBuddyImplOkHttp example = new BikeBuddyImplOkHttp();
         try {
           Bikes[] bikesList = example.getAllBikes();
-            System.out.println("Length"+bikesList.length);
-            for(Bikes bike : bikesList){
-              System.out.println(bike.toString());
-          }
+            //System.out.println("Length"+bikesList.length);
+//            for(Bikes bike : bikesList){
+//             // System.out.println(bike.toString());
+//          }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
-
-
-
+//
+//
+//
+//}
+//
+//public class BikeBuddyImplOkHttp {
+//    private static List<Bikes> bikes = new ArrayList<>();
+//
+//    public BikeBuddyImplOkHttp() {
+//        bikes.add(new Bikes(1l));
+//        bikes.add(new Bikes(2l));
+//    }
+//
+//
+//   // @RequestMapping(value = "/bikes", method = RequestMethod.GET)
+//    public ResponseEntity<Iterable<Bikes>> getAllBooks() {
+//        return new ResponseEntity<>(bikes, HttpStatus.OK);
+//    }
 }
 
 
--- app/src/main/java/com/example/sfldpguser_24/model/Bikes.java
+++ app/src/main/java/com/example/sfldpguser_24/model/Bikes.java
@@ -3,17 +3,74 @@ package com.example.sfldpguser_24.model;
 /**
  * Created by SFLDPGUSER-24 on 3/2/2018.
  */
-
+//@XmlRootElement(name = "bikes")
 public class Bikes {
-    private String empty_slots = "";
-    private String free_bikes = "";
+        private String nameOfStation;
+   // private String free_bikes = "";
 //    private String id;
 //    private String latitude;
 //    private String longitude;
+      public Bikes() {
+      }
+    // getter
 
+    public Bikes(String nameOfStation) {
+        super();
+        this.nameOfStation = nameOfStation;
+    }
 
+    /**
+     * @return the id
+     */
+    public String nameOfStation() {
+        return nameOfStation;
+    }
+
+    /**
+     * @param nameOfStation the empty_slots to set
+     */
+    public void setnameOfStation(String nameOfStation) {
+        this.nameOfStation = nameOfStation;
+    }
+
+//    /**
+//     * @return the name
+//     */
+//    public String getName() {
+//        return name;
+//    }
+//
+//    /**
+//     * @param name the name to set
+//     */
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    /**
+//     * @return the author
+//     */
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    /**
+//     * @param author the author to set
+//     */
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    /*
+//     * (non-Javadoc)
+//     *
+//     * @see java.lang.Object#toString()
+//     */
     @Override
     public String toString() {
-        return empty_slots;
+        return "Bike [nameOfStation=" + nameOfStation + "]";
     }
+
 }
+
+
