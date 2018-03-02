a32e7de8c52bd6ee75cfc0f2196ae03f5646e265
SFLDPGUSER-24 <maulik.shah9999@gmail.com>
2018-03-02 13:53:41 -0500

    Created the POJO and Connection classes

--- app/src/main/java/com/example/sfldpguser_24/model/BikeBuddyImplOkHttp.java
+++ app/src/main/java/com/example/sfldpguser_24/model/BikeBuddyImplOkHttp.java
@@ -4,32 +4,38 @@ package com.example.sfldpguser_24.model;
  * Created by SFLDPGUSER-24 on 3/2/2018.
  */
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class BikeBuddyImplOkHttp {
-    private static final String URI_BIKES = "http://10.5.5.12:8080/test";
+    private static final String URI_BIKES = "http://10.5.5.12:8080/getLocations?type=pickOff&latitude=40.7143528&longitude=-74.00597309999999";
 
     public Bikes[] getAllBikes() throws Exception {
-        Bikes[] bikesList = null;
+        Bikes[] bikes = null;
         OkHttpClient httpclient = new OkHttpClient();
         Request request = new Request.Builder().url(URI_BIKES).get().build();
         try (Response response = httpclient.newCall(request).execute()) {
             ObjectMapper mapper = new ObjectMapper();
-            bikesList = mapper.readValue(response.body().bytes(), Bikes[].class);
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            bikes = mapper.readValue(response.body().bytes(), Bikes[].class);
         }
-        return bikesList;
+        return bikes;
     }
     public static void main(String[] args) throws IOException {
         BikeBuddyImplOkHttp example = new BikeBuddyImplOkHttp();
         try {
           Bikes[] bikesList = example.getAllBikes();
-            System.out.println(bikesList);
+            System.out.println("Length"+bikesList.length);
+            for(Bikes bike : bikesList){
+              System.out.println(bike.toString());
+          }
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ app/src/main/java/com/example/sfldpguser_24/model/Bikes.java
@@ -0,0 +1,19 @@
+package com.example.sfldpguser_24.model;
+
+/**
+ * Created by SFLDPGUSER-24 on 3/2/2018.
+ */
+
+public class Bikes {
+    private String empty_slots = "";
+    private String free_bikes = "";
+//    private String id;
+//    private String latitude;
+//    private String longitude;
+
+
+    @Override
+    public String toString() {
+        return empty_slots;
+    }
+}
